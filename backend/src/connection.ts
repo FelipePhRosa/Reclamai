0c3bcb8f2f7b262efb98b7759a63758668905604
J-TheCreator <joaovitormartins23881@gmail.com>
2025-07-05 12:09:21 -0300

    adição do .env na conexão knex

--- backend/src/connection.ts
+++ backend/src/connection.ts
@@ -2,13 +2,13 @@ require('dotenv').config();
 import knex from 'knex'
 
 const connection = knex({
-  client: 'mysql2',
+  client: process.env.DB_CONNECTION || "mysql2",
   connection: {
-    host: 'localhost',
-    user: 'root',
-    port: 3306,
-    password: '801680',
-    database: 'ottpel' 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: Number(process.env.DB_PORT),
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   },
 })
 export default connection
