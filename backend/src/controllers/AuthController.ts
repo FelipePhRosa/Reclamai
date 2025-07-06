447458a2edbe10f2eebbf9be17bd4d43edd0126b
Felipe Rosa <ofcFelipeRosa@gmail.com>
2025-07-05 21:25:35 -0300

    Auth: Controller Login Auth

---
+++ backend/src/controllers/AuthController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import AuthService from "../Services/authService";
+
+export default class AuthController {
+  constructor(private authService = new AuthService()) {}
+
+  async login(req: Request, res: Response) {
+    console.log("Login Request Body:", req.body);
+    const { email, password } = req.body;
+
+    try {
+      const token = await this.authService.login({ email, password });
+        res.status(200).json({ token });
+    } catch (error) {
+      console.error(error);
+        res.status(401).json({ error: "Invalid credentials" });
+    }
+  }
+}
