a838ffa8a5338118eed0c217c7d43a37285da523
Felipe Rosa <ofcFelipeRosa@gmail.com>
2025-07-22 20:46:53 -0300

    Feature: Like's in Reports SQL

--- backend/src/services/reportService.ts
+++ backend/src/services/reportService.ts
@@ -16,9 +16,30 @@ export default class ReportService {
         return await connection('reports').insert(reportData);
     }
 
-    async getAllReports(){
-        return await connection('reports').where({ status: 'aprovado'}).select('*');
+    async getAllReports(userId?: number) {
+      const reports = await connection('reports')
+        .where({ status: 'aprovado' })
+        .select(
+          'reports.*',
+          // Subquery: total de likes
+          connection('likes')
+            .count('*')
+            .whereRaw('likes.report_id = reports.id')
+            .as('likes'),
+
+          // Subquery: se o usuário curtiu
+          connection.raw(
+            `(SELECT EXISTS (
+                SELECT 1 FROM likes 
+                WHERE likes.report_id = reports.id AND likes.user_id = ?
+            )) as likedByCurrentUser`,
+            [userId ?? 0]
+          )
+        );
+
+      return reports;
     }
+
 
     async getAllReportsPending(){
         return await connection('reports').where({ status: 'pendente' }).select('*');
@@ -34,5 +55,9 @@ export default class ReportService {
 
     async deleteReport(reportId: number){
         return await connection('reports').where({ id: reportId }).delete();
+    }
+
+    async getAllLikes(reportId: number){
+        return await connection('likes').where({ report_id: reportId }).count('* as total');
     }
 }
