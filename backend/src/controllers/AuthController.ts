7f2b5db496672013ca215f4ce26ba3e88a2bd773
Felipe Rosa <ofcFelipeRosa@gmail.com>
2025-07-22 20:42:58 -0300

    Update: Email/User identifier for login

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,19 +1,21 @@
 import { Request, Response } from "express";
-import AuthService from "../Services/authService";
+import AuthService from "../services/authService";
 
 export default class AuthController {
   constructor(private authService = new AuthService()) {}
 
   async login(req: Request, res: Response) {
     console.log("Login Request Body:", req.body);
-    const { email, password } = req.body;
+    const { identifier, password } = req.body;
 
     try {
-      const token = await this.authService.login({ email, password });
-        res.status(200).json({ token });
+      const loginResult = await this.authService.login({ identifier, password });
+      // loginResult = { userId, name, email, role, token }
+      
+      res.status(200).json(loginResult);  // envia tudo para o frontend
     } catch (error) {
       console.error(error);
-        res.status(401).json({ error: "Invalid credentials" });
-    }
-  }
+      res.status(401).json({ error: "Invalid credentials" });
+    }
+  }
 }
