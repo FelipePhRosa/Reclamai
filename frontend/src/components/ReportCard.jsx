cd09ebc4c1fa945ac20e653dca26582c956b22ad
Felipe Rosa <ofcFelipeRosa@gmail.com>
2025-07-22 20:58:49 -0300

    Implement: Card component was been implemented

--- frontend/src/components/ReportCard.jsx
+++ frontend/src/components/ReportCard.jsx
@@ -1,6 +1,20 @@
-import { Clock5, ThumbsUp, MessageSquare, Droplet, CircleDashed, Lightbulb } from 'lucide-react';
+import { Clock5, ThumbsUp, MessageSquare } from "lucide-react";
+import { LikeButton } from "./LikeButton"; // ajuste caminho
 
-function ReportCard({ image, status, statusColor, icon, category, title, address, time, likes, comments }) {
+function ReportCard({
+  id,
+  image,
+  status,
+  statusColor,
+  icon,
+  category,
+  title,
+  address,
+  time,
+  likes,
+  comments,
+  likedByCurrentUser,
+}) {
   return (
     <div className="flex items-center border rounded-md shadow-sm overflow-hidden bg-white mt-5 min-h-[150px] pl-5">
       {/* Imagem */}
@@ -10,11 +24,13 @@ function ReportCard({ image, status, statusColor, icon, category, title, address
       <div className="p-4 flex flex-col justify-between flex-1">
         <div>
           {/* Status e Categoria */}
-          <div className="flex items-center">
-            <span className={`bg-${statusColor}-100 text-${statusColor}-600 text-xs font-semibold  py-0.5 rounded-full`}>
+          <div className="flex items-center gap-2 mb-2">
+            <span
+              className={`bg-${statusColor}-100 text-${statusColor}-600 text-xs font-semibold py-0.5 rounded-full`}
+            >
               {status}
             </span>
-            <div className="flex items-center">
+            <div className="flex items-center text-gray-600 text-xs font-medium px-4 py-0.5 rounded-full">
               {icon}
               {category}
             </div>
@@ -31,9 +47,13 @@ function ReportCard({ image, status, statusColor, icon, category, title, address
             <Clock5 size={12} />
             <span>{time}</span>
           </div>
-          <div className="flex items-center gap-1">
-            <ThumbsUp size={12} />
-            <span>{likes}</span>
+          <div className="flex items-center gap-3">
+            {/* Removemos o ícone e texto fixo de likes aqui */}
+            <LikeButton
+              reportId={id}
+              initialLiked={likedByCurrentUser}
+              initialLikes={likes}
+            />
           </div>
           <div className="flex items-center gap-1">
             <MessageSquare size={12} />
