0e5d6eaafa15e56cbe0e02374c0910f2eda1cd38
Felipe Rosa <ofcFelipeRosa@gmail.com>
2025-08-15 20:24:33 -0300

    Map Total View

--- frontend/src/pages/MapaProblemas.jsx
+++ frontend/src/pages/MapaProblemas.jsx
@@ -1,6 +1,5 @@
 import Layout from "../components/Layout";
 import MapaDenuncia from "../components/MapaLeaflet";
-import AlertaRodape from "../components/Rodape";
 import 'leaflet/dist/leaflet.css';
 
 function MapaProblemas() {
@@ -9,9 +8,6 @@ function MapaProblemas() {
       <div className="flex flex-col h-full">
         <div className="flex-1">
           <MapaDenuncia />
-        </div>
-        <div className="flex items-center justify-center mt-4">
-          <AlertaRodape />
         </div>
       </div>
     </Layout>
