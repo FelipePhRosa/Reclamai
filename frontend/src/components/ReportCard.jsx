80f602a91629cb7d3beb9a11e285577f02162ca1
Felipe Rosa <ofcFelipeRosa@gmail.com>
2025-08-22 18:47:50 -0300

    Implementation: New Style with DarkMode

--- frontend/src/components/ReportCard.jsx
+++ frontend/src/components/ReportCard.jsx
@@ -1,4 +1,4 @@
-import { Clock5, ThumbsUp, MessageSquare } from "lucide-react";
+import { Clock5, ThumbsUp, MessageSquare, ArrowDownRight } from "lucide-react";
 import { LikeButton } from "./LikeButton"; // ajuste caminho
 
 function ReportCard({
@@ -16,7 +16,7 @@ function ReportCard({
   likedByCurrentUser,
 }) {
   return (
-    <div className="flex items-center rounded-md shadow-sm overflow-hidden bg-white mt-5 min-h-[150px] pl-5">
+    <div className="flex items-center rounded-md shadow-sm overflow-hidden bg-white mt-5 min-h-[150px] pl-5 dark:bg-gray-800">
       {/* Imagem */}
       <img src={image} alt={title} className="h-32 w-32 object-cover rounded-xl" />
 
@@ -26,7 +26,7 @@ function ReportCard({
           {/* Status e Categoria */}
           <div className="flex items-center gap-2 mb-2">
             <span
-              className={`bg-${statusColor}-100 text-${statusColor}-600 text-xs font-semibold p-2 rounded-full`}
+              className={`bg-${statusColor}-100 text-${statusColor}-600 text-xs font-semibold p-1.5 rounded-full capitalize`}
             >
               {status}
             </span>
@@ -37,14 +37,14 @@ function ReportCard({
           </div>
 
           {/* Título e Endereço */}
-          <h2 className="font-bold text-xl text-gray-800">{title}</h2>
-          <p className="text-gray-500 text-md mt-1">{address}</p>
+          <h2 className="font-bold text-xl text-gray-800 dark:text-gray-300">{title}</h2>
+          <p className="text-gray-500 text-md mt-1 dark:text-gray-400">{address}</p>
         </div>
 
         {/* Informações Adicionais */}
-        <div className="flex items-center gap-3 mt-2 text-sm text-gray-600">
+        <div className="flex items-center gap-3 mt-2 text-sm text-gray-600 dark:text-gray-300 dark:font-semibold">
           <div className="flex items-center gap-1">
-            <Clock5 size={12} />
+            <Clock5 size={14} />
             <span>{time}</span>
           </div>
           <div className="flex items-center gap-3">
@@ -56,9 +56,12 @@ function ReportCard({
             />
           </div>
           <div className="flex items-center gap-1">
-            <MessageSquare size={12} />
+            <MessageSquare size={14} />
             <span>{comments}</span>
           </div>
+          <button className="ml-auto w-10 h-10 bg-blue-500 rounded-xl flex items-center justify-center cursor-pointer">
+            <ArrowDownRight className="text-white "/>
+          </button>
         </div>
       </div>
     </div>
