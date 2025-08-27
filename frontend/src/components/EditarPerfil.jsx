0f06aa9a717f3ed6fcdaa6e5acdd668b83453f18
Felipe Rosa <ofcFelipeRosa@gmail.com>
2025-08-27 19:14:01 -0300

    Improvements: Added fullName view & DarkMode

--- frontend/src/components/EditarPerfil.jsx
+++ frontend/src/components/EditarPerfil.jsx
@@ -1,5 +1,5 @@
 import Layout from "./Layout";
-import { Camera, Pencil } from "lucide-react";
+import { Trash2, AtSign, Camera, Pencil, Mail, Phone, ALargeSmall, Calendar1, UserRound, KeyRound } from "lucide-react";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
@@ -11,27 +11,27 @@ export default function EditarPerfil() {
     return cpf.replace(/^(\d{3})\d{3}(\d{3})(\d{2})$/, "$1.***.$2-**");
   }
   const primeiroNome = user?.nameUser ? user.nameUser.split(" ")[0] : "Usuário";
-  const EmailUser = user?.email || "email";
-  const TelefoneUser = user?.telefone || "telefone";
+  const segundoNome = user?.fullName || 'John Wick';
+  const EmailUser = user?.email || "E-mail";
+  const TelefoneUser = user?.telefone || "Número de Celular";
   const CpfUser = mascararCPF(user?.cpf);
   
 
 
 return (
   <Layout>
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="bg-white w-[450px] p-6 rounded-lg shadow">
+    <div className="flex justify-center items-center bg-gray-100 dark:bg-gray-900">
+      <div className="bg-transparent w-[100%] h-[100%] p-8 rounded-lg lg:w-[30%]">
         <div className="flex gap-1">
-          <h1 className="font-bold text-blue-500 text-2xl mb-4">Editar Perfil</h1>
-          <Pencil className="h-8 text-blue-500" />
+          <h1 className="font-bold text-black text-2xl mb-4 dark:text-white">Editar Perfil</h1>
         </div>
 
-        <div className="flex flex-col items-center gap-4 py-4">
-          <h2 className="text-lg mb-2 font-semibold">Foto de perfil</h2>
+        <div className="flex flex-col items-center gap-5">
+          <h2 className="text-xl font-semibold dark:text-white">Foto de perfil</h2>
 
           <div className="relative w-32 h-32">
             <img
-              src={user?.avatar_url || "/default-avatar.png"}
+              src={user?.avatar_url || "https://w7.pngwing.com/pngs/177/551/png-transparent-user-interface-design-computer-icons-default-stephen-salazar-graphy-user-interface-design-computer-wallpaper-sphere-thumbnail.png"}
               alt=""
               className="w-full h-full rounded-full object-cover border border-gray-400 bg-gray-100"
             />
@@ -39,37 +39,76 @@ return (
               <Camera size={20} className="text-gray-600" />
             </div>
           </div>
-          <p className="text-lg font-medium text-gray-800">{primeiroNome}</p>
         </div>
 
         <div className="text-center" />
 
-        <div className="flex flex-col gap-2">
-          <h2 className="text-blue-500 font-bold text-center">Informações do usuário</h2>
-
-          <div>
-            <h2 className="font-semibold text-blue-500">Email:</h2>
-            <p className="text-sm text-gray-500">{EmailUser}</p>
-            <hr className="my-4 border-gray-300" />
+        <div className="flex flex-col gap-4 mt-2">
+          <div className="bg-gray-100 border border-gray-400 shadow-lg rounded-xl p-4 flex flex-col gap-5 mt-2 dark:bg-gray-700 dark:shadow-gray-600">
+            <div className="flex gap-2 items-center">
+                <UserRound className="dark:text-white "/>
+                  <div className="flex flex-col">
+                    <h2 className="font-semibold text-gray-700 dark:text-gray-100">Nome:</h2>
+                    <p className="text-sm text-gray-600 dark:text-gray-300">{segundoNome}</p>
+                  </div>
+            </div>
+            <div className="flex gap-2 items-center">
+                <AtSign className="dark:text-white"/>
+                  <div className="flex flex-col">
+                    <h2 className="font-semibold text-gray-700 dark:text-gray-100">Nome de Usuário:</h2>
+                    <p className="text-sm text-gray-600 dark:text-gray-300">{primeiroNome}</p>
+                  </div>
+            </div>
           </div>
+        </div>
 
-          <div>
-            <h2 className="font-semibold text-blue-500">Celular:</h2>
-            <p className="text-sm text-gray-500">{TelefoneUser}</p>
-            <hr className="my-4 border-gray-300" />
+        <div className="flex flex-col gap-4">
+        <h2 className="text-black font-semibold mt-5 text-xl dark:text-white">Informações Privadas</h2>
+          <div className="bg-gray-100 border border-gray-400 shadow-xl rounded-xl p-6 flex flex-col gap-5 dark:bg-gray-700 dark:shadow-gray-600">
+
+            <div className="flex gap-2 items-center">
+              <KeyRound className="dark:text-white"/>
+              <div className="flex flex-col">
+                <h2 className="font-semibold text-gray-700 dark:text-gray-100">Senha:</h2>
+                <p className="text-lg font-bold text-gray-600 dark:text-gray-300">*************</p>
+              </div>
+            </div>
+
+            <div className="flex gap-2 items-center">
+              <Calendar1 className="dark:text-white"/>
+              <div className="flex flex-col">
+                <h2 className="font-semibold text-gray-700 dark:text-gray-100">Data de Nascimento</h2>
+                <p className="text-sm text-gray-600 dark:text-gray-300">21/11/2003</p>
+              </div>
+            </div>
+            
+            <div className="flex gap-2 items-center ">
+              <Mail className="dark:text-white"/>
+              <div className="flex flex-col">
+                <h2 className="font-semibold text-gray-700 dark:text-gray-100">Email:</h2>
+                <p className="text-sm text-gray-600 dark:text-gray-300">{EmailUser}</p>
+              </div>
+            </div>
+
+            <div className="flex gap-2 items-center">
+              <Phone className="dark:text-white"/>
+              <div className="flex flex-col">
+                <h2 className="font-semibold text-gray-700 dark:text-gray-100">Celular:</h2>
+                <p className="text-sm text-gray-600 capitalize dark:text-gray-300">{TelefoneUser}</p>
+              </div>
+            </div>
+
+            <div>
+              <button className="text-blue-500 font-semibold hover:text-blue-300 hover:cursor-pointer">
+                Redefinir senha
+              </button>
+            </div>
           </div>
-
-          <div>
-            <h2 className="font-semibold text-blue-500">Cpf:</h2>
-            <p className="text-sm text-gray-500">{CpfUser}</p>
-            <hr className="my-4 border-gray-300" />
-          </div>
-
-          <div>
-            <button className="text-blue-500 font-semibold hover:text-blue-300 hover:cursor-pointer">
-              Redefinir senha
-            </button>
-            <hr className="my-4 border-gray-300" />
+          <div className="flex justify-center items-center mb-20 mt-10">
+            <div className="flex border rounded-xl p-3 hover:bg-red-500 hover:text-white transition-all duration-105 dark:bg-red-500 dark:border-gray-3 00">
+              <Trash2 className="dark:text-white"/>
+              <h2 className="font-semibold ml-2 dark:text-white">Deletar Conta</h2>
+            </div>
           </div>
         </div>
       </div>
