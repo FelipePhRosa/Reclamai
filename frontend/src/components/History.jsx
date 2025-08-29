068311f764b11c683b980f33089c5b2b32015e00
Felipe Rosa <ofcFelipeRosa@gmail.com>
2025-08-29 01:08:43 -0300

    Feature: New History all reports approved for user

--- frontend/src/components/History.jsx
+++ frontend/src/components/History.jsx
@@ -4,6 +4,7 @@ import { formatDistanceToNow } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { Undo2 } from 'lucide-react';
 import { Link } from 'react-router-dom';
+import Layout from './Layout';
 
 function History() {
   const [userReports, setUserReports] = useState([]);
@@ -25,20 +26,12 @@ function History() {
       },
     })
       .then((response) => {
-        if (!response.ok) {
-          throw new Error('Erro ao buscar os relatos do usuário.');
-        }
+        if (!response.ok) throw new Error('Erro ao buscar os relatos do usuário.');
         return response.json();
       })
-      .then((data) => {
-        setUserReports(data.data || []);
-      })
-      .catch((error) => {
-        console.error('Erro ao buscar o histórico:', error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+      .then((data) => setUserReports(data.data || []))
+      .catch((error) => console.error('Erro ao buscar o histórico:', error))
+      .finally(() => setLoading(false));
   }, []);
 
   if (loading) {
@@ -50,42 +43,40 @@ function History() {
   }
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 dark:bg-gray-900 p-6">
-        <div className='flex gap-2 '>
-        <Link to ="/settings"><Undo2  className='text-blue-500 h-10'/>
-         </Link>
-      <h1 className="text-3xl font-bold mb-6 dark:text-gray-200">Histórico de Denúncias</h1>
-        </div>
-         
-
-      {userReports.length === 0 ? (
-        <p className="text-center text-gray-500 mb-6">
-          Você ainda não fez nenhuma denúncia.
-        </p>
-      ) : (
-        <div className="w-full max-w-md overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200" style={{ maxHeight: '60vh' }}>
-          {userReports.map((report) => (
-            <ReportCard
-              key={report.id}
-              id={report.id}
-              image={report.image}
-              status={report.status}
-              statusColor="green"
-              category={report.category}
-              title={report.reportTitle}
-              address={report.address}
-              time={formatDistanceToNow(new Date(report.updated_at), {
-                addSuffix: true,
-                locale: ptBR,
-              })}
-              likes={report.likes}
-              likedByCurrentUser={report.likedByCurrentUser}
-              comments={1}
-            />
-          ))}
-        </div>
-      )}
-    </div>
+    
+      <div className="flex w-full max-w-screen">
+        {userReports.length === 0 ? (
+          <p className="text-gray-500 text-center">
+            Você ainda não fez nenhuma denúncia.
+          </p>
+        ) : (
+          <div>
+            <h1 className='text-3xl font-bold dark:text-gray-200'>Suas denúncias:</h1>
+            <div className="grid grid-cols-3 gap-4 text-sm">
+              {userReports.map((report) => (
+                <ReportCard
+                  key={report.id}
+                  id={report.id}
+                  image={report.image}
+                  status={report.status}
+                  statusColor="green"
+                  category={report.category}
+                  title={report.reportTitle}
+                  address={report.address}
+                  time={formatDistanceToNow(new Date(report.updated_at), {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })}
+                  likes={report.likes}
+                  likedByCurrentUser={report.likedByCurrentUser}
+                  comments={1}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    
   );
 }
 
