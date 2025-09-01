e6d8eb3596bde7d8b66bb9d484c9592a75113ea9
Felipe Rosa <ofcFelipeRosa@gmail.com>
2025-08-31 21:06:12 -0300

    Update

--- frontend/src/components/ReportCard.jsx
+++ frontend/src/components/ReportCard.jsx
@@ -1,5 +1,6 @@
 import { Clock5, ThumbsUp, MessageSquare, ArrowDownRight } from "lucide-react";
 import { LikeButton } from "./LikeButton"; // ajuste caminho
+import { Link } from "react-router-dom";
 
 function ReportCard({
   id,
@@ -16,13 +17,17 @@ function ReportCard({
   likedByCurrentUser,
 }) {
   return (
-    <div className="flex items-center rounded-md shadow-sm overflow-hidden bg-white mt-5 min-h-[150px] pl-5 dark:bg-gray-800">
+    <div className="flex items-stretch rounded-md shadow-sm overflow-hidden bg-white mt-5 max-h-[200px] pl-5 dark:bg-gray-800">
       {/* Imagem */}
-      <img src={image} alt={title} className="h-32 w-32 object-cover rounded-xl" />
+        <img 
+          src={image} 
+          alt={title} 
+          className="h-32 w-32 object-cover rounded-xl my-auto" 
+        />
 
       {/* Conteúdo */}
-      <div className="p-4 flex flex-col justify-between flex-1">
-        <div>
+      <div className="p-5 flex flex-col justify-between flex-1 min-h-0">
+        <div className="flex-1">
           {/* Status e Categoria */}
           <div className="flex items-center gap-2 mb-2">
             <span
@@ -30,25 +35,28 @@ function ReportCard({
             >
               {status}
             </span>
-            <div className="flex items-center text-gray-600 text-xs font-medium px-4 py-0.5 rounded-full">
+            <div className="flex items-center text-gray-600 text-xs font-medium rounded-full">
               {icon}
               {category}
             </div>
           </div>
 
           {/* Título e Endereço */}
-          <h2 className="font-bold text-xl text-gray-800 dark:text-gray-300">{title}</h2>
-          <p className="text-gray-500 text-md mt-1 dark:text-gray-400">{address}</p>
+          <h2 className="font-bold text-xl text-gray-800 dark:text-gray-300 line-clamp-2">
+            {title}
+          </h2>
+          <p className="text-gray-500 text-md mt-1 dark:text-gray-400 line-clamp-1">
+            {address}
+          </p>
         </div>
 
         {/* Informações Adicionais */}
-        <div className="flex items-center gap-3 mt-2 text-sm text-gray-600 dark:text-gray-300 dark:font-semibold">
+        <div className="flex items-center gap-2 text-sm text-gray-600 dark:text-gray-300 dark:font-semibold flex-shrink-0">
           <div className="flex items-center gap-1">
             <Clock5 size={14} />
             <span>{time}</span>
           </div>
           <div className="flex items-center gap-3">
-            {/* Removemos o ícone e texto fixo de likes aqui */}
             <LikeButton
               reportId={id}
               initialLiked={likedByCurrentUser}
@@ -59,9 +67,11 @@ function ReportCard({
             <MessageSquare size={14} />
             <span>{comments}</span>
           </div>
-          <button className="ml-auto w-10 h-10 bg-blue-500 rounded-xl flex items-center justify-center cursor-pointer">
-            <ArrowDownRight className="text-white "/>
-          </button>
+          <Link to={`/report/${id}`} className="ml-auto w-10 h-8">
+            <button className="ml-auto w-10 h-10 bg-blue-500 rounded-xl flex items-center justify-center cursor-pointer flex-shrink-0">
+              <ArrowDownRight className="text-white"/>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
