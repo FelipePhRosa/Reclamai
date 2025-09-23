f95a2a2de1c9a030ff982fe70c975efd63f91df9
Felipe Rosa <ofcFelipeRosa@gmail.com>
2025-09-23 17:03:49 -0300

    Default.

--- backend/src/connection.ts
+++ backend/src/connection.ts
@@ -4,11 +4,11 @@ import knex from 'knex'
 const connection = knex({
   client: process.env.DB_CONNECTION || "mysql2",
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    port: Number(process.env.DB_PORT),
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "aluno",
+    port: Number(process.env.DB_PORT) || 3306,
+    password: process.env.DB_PASSWORD || "senacrs",
+    database: process.env.DB_NAME || "reclamai",
   },
 })
 export default connection
