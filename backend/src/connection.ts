74dba0b03e1980868a74f79063d5907c8068641a
Felipe Rosa <ofcFelipeRosa@gmail.com>
2025-10-16 12:19:26 -0300

    Edit

--- backend/src/connection.ts
+++ backend/src/connection.ts
@@ -5,7 +5,7 @@ const connection = knex({
   client: process.env.DB_CONNECTION || "mysql2",
   connection: {
     host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "aluno",
+    user: process.env.DB_USER || "root",
     port: Number(process.env.DB_PORT) || 3306,
     password: process.env.DB_PASSWORD || "senacrs",
     database: process.env.DB_NAME || "reclamai",
