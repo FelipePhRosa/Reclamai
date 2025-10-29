e079b12a5897f4225ab9b08e0afbb9885e707741
Felipe Rosa <ofcFelipeRosa@gmail.com>
2025-10-29 12:41:31 -0300

    Added: Verification Email.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,30 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
+const ClientId = import.meta.env.VITE_CLIENTE_ID;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
-
-
-
- <App/>
-
-
-
-{/*
-<ChatComunidade/>
- <MapaProblemas/>
- <Report/>
-<Problemas/>
-<App />
-<Sidebar/>
-
-
-*/}
-
-
-
+    <GoogleOAuthProvider clientId={ClientId}>
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
-)
+);
