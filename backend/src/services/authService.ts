c85fb26fff86f07caeaf8ff950bc26814a7b4d6b
Felipe Rosa <ofcFelipeRosa@gmail.com>
2025-10-29 22:57:56 -0300

    Feature: Now possible GoogleLogin.

--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -7,6 +7,15 @@ interface LoginCredentials {
   password: string;
 }
 
+interface SocialLoginCredentials {
+  email: string;
+  name: string;
+  fullName: string;
+  avatar: string;
+  provider: 'google' | "facebook" | "github" | "apple";
+  providerId: string;
+}
+
 interface TokenPayload {
   userId: number;
   email: string;
@@ -14,7 +23,6 @@ interface TokenPayload {
   role: number;
   avatar_url: string;
   telefone: string;
-  city_id: number;
 }
 
 export default class AuthService {
@@ -71,8 +79,7 @@ export default class AuthService {
         fullName: user.fullName,
         role: user.role,
         avatar_url: user.avatar_url,
-        telefone: user.telefone,
-        city_id: user.city_id
+        telefone: user.telefone
       });
 
       return {
@@ -81,10 +88,10 @@ export default class AuthService {
         fullName: user.fullName,
         email: user.email,
         telefone: user.telefone,
+        birth_date: user.birth_date,
         cpf: user.cpf,
         role: user.role,
         avatar_url: user.avatar_url,
-        city_id: user.city_id,
         token,
       };
     } catch (error) {
@@ -92,4 +99,114 @@ export default class AuthService {
       throw error;
     }
   }
+
+async socialLogin(credentials: SocialLoginCredentials) {
+    try {
+      console.log("Login Attemp: ", credentials);
+
+      if (
+        !credentials ||
+        !credentials.email ||
+        !credentials.avatar ||
+        !credentials.name ||
+        !credentials.provider ||
+        !credentials.providerId
+      ) {
+        console.log("Something is missing");
+        throw new Error("Missing required fields");
+      }
+
+        const emailClear = credentials.email.trim().toLowerCase();
+        const fullName = credentials.fullName;
+        const nameUser = (credentials.name || "").trim().split(" ")[0] || "Usuário";
+        let user = await connection("users").whereRaw("lower(trim(email)) = ?", [emailClear]).first();
+
+        if (user) {
+          if (user.provider === "local") {
+            await connection("users")
+              .where("id", user.id)
+              .update({
+                provider: credentials.provider,
+                provider_id: credentials.providerId,
+                nameUser: nameUser,
+                fullName: fullName,
+                avatar_url: credentials.avatar,
+              });
+            user = await connection("users").where("id", user.id).first()
+            console.error(`User connection on with: ${credentials.provider}`)
+          } else if (user.provider === credentials.provider) {
+            console.log("Existing social user login: ", user.provider)
+
+          } else {
+            throw new Error(`Account already registered with provider '${user.provider}'. Please sign in using that provider or link accounts.`);
+          }
+        } else {
+          const [newUserId] = await connection("users").insert({
+            nameUser: nameUser,
+            fullName: fullName,
+            email: emailClear,
+            password_hash: "",
+            provider: credentials.provider,
+            provider_id: credentials.providerId,
+            avatar_url: credentials.avatar,
+            created_at: new Date(),
+            role: 5,
+            birth_date: new Date(), // Usa data atual como fallback quando Google não fornece
+          });
+
+          user = await connection("users").where("id", newUserId).first();
+          if (!user) {
+            console.log("Error to create user: ", credentials.name)
+          } else {
+            console.log("Created New User: ", user);
+          }
+        }
+      const token = this.generateToken({
+        userId: user.id,
+        email: user.email,
+        fullName: user.fullName,
+        role: user.role,
+        avatar_url: user.avatar_url,
+        telefone: user.telefone
+      });
+
+      return {
+        userId: user.id,
+        nameUser: user.nameUser,
+        fullName: credentials.fullName,
+        email: user.email,
+        role: user.role,
+        avatar_url: user.avatar_url,
+        provider: user.provider,
+        token,
+        isNewUser: !user.created_at || new Date().getTime() - new Date(user.created_at).getTime() < 5000
+      };
+    } catch (err) {
+      console.log("Error when logging in", err)
+      throw err;
+    }
+  }
+
+  async verifySocialToken(provider: string, token: string): Promise<boolean> {
+    try {
+      switch (provider) {
+        case 'google':
+          const googleResponse = await fetch(
+            `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${token}`
+          );
+          return googleResponse.ok;
+          //For now, we will only use Google Login, since we don’t have a secured domain.
+          case 'facebook':
+            const fbResponse = await fetch(
+              `https://graph.facebook.com/me?access_token=${token}` 
+            );
+          return fbResponse.ok;
+
+        default:
+          return false;
+      }
+    } catch {
+      return false;
+    }
+  }
 }
