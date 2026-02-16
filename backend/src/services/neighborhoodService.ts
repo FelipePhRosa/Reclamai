f56122236920530296f1d7116db773094916c4ea
Felipe Rosa <ofcFelipeRosa@gmail.com>
2026-02-16 13:21:22 -0300

    new - get's for data collection

--- backend/src/services/neighborhoodService.ts
+++ backend/src/services/neighborhoodService.ts
@@ -21,7 +21,26 @@ export default class NeighborHoodService {
         return await connection('neighborhoods').where({ city_id: city_id }).select('*')
     }
 
+    async getAllInfoNeighborhood(neighborhood_id: Number, city_id: Number){
+        return await connection('neighborhoods').where({ id: neighborhood_id, city_id: city_id }).select('*')
+    }
     async delNeighborhoodById(neighborhood_id: Number, city_id: Number){
         return await connection('neighborhoods').where({ id: neighborhood_id, city_id: city_id }).delete();
+    }
+
+    async TotalAccountInNeighborhood(neighborhood_id: Number, city_id: Number){
+        return await connection('users').where({ neighborhood_id, city_id }).count('* as totalAccounts');
+    }
+
+    async getAllAccountsInNeighborhood(neighborhood_id: Number, city_id: Number){
+        return await connection('users').where({ neighborhood_id, city_id }).select('*');
+    }
+
+    async getAllReportsByNeighborhood(neighborhood_id: Number, city_id: Number){
+        return await connection('reports').where({ neighborhood_id, city_id }).orderBy('created_at', 'desc');
+    }
+
+    async getTotalReportsByNeighborhood(neighborhood_id: Number, city_id: Number){
+        return await connection('reports').where({ neighborhood_id: neighborhood_id, city_id: city_id }).count('* as totalReport');
     }
 }
